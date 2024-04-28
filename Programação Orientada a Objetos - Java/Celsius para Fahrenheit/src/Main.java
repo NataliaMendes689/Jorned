7a4741ba4542efc37d4d02cf05c525b7d7335e31
NataliaMelo689 <126292665+NataliaMendes689@users.noreply.github.com>
2024-04-28 15:56:41 -0300

    edit

---
+++ Programação Orientada a Objetos - Java/Celsius para Fahrenheit/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int tempC = 30;
+        int tempF = (int) (tempC * 1.8) + 32;
+
+        System.out.println( tempC + " graus Celsius equivalem a " + tempF + " graus Fahrenheit.");
+
+        }
+    }
