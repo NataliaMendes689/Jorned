c9718c26c6baad27acc2131c8c44478a6b7c70a2
Natalia Mendes <126292665+NataliaMendes689@users.noreply.github.com>
2024-08-08 20:26:07 -0300

    Update app.js

--- Lógica de Programação - JavaScript/Mergulhe com JavaScript - Jogo do N° Secreto/logica-js-projeto_inicial/app.js
+++ Lógica de Programação - JavaScript/Mergulhe com JavaScript - Jogo do N° Secreto/logica-js-projeto_inicial/app.js
@@ -5,10 +5,8 @@ console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
-// enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
     chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-    // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
     } else {
@@ -17,16 +15,9 @@ while (chute != numeroSecreto) {
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
-        // tentativas = tentativas + 1;
         tentativas++;
     }
 }
 
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
 alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
-
-// if (tentativas > 1) {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-// } else {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-// }
